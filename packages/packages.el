dc95c5078851c0f7ea5d3e8e8123fb4dd7a5c77d
BzFTMxc <BzFTMxc@icloud.com>
2019-08-08 15:35:58 +0700

    Added scala mode, sbt mode and ensime

--- packages/packages.el
+++ packages/packages.el
@@ -19,6 +19,6 @@
 (or (file-exists-p package-user-dir)
     (package-refresh-contents))
 
-(ensure-package-installed 'magit 'yasnippet 'projectile 'helm 'helm-projectile 'multiple-cursors 'dracula-theme 'monokai-theme 'groovy-mode 'meghanada 'dumb-jump 'go-mode 'protobuf-mode)
+(ensure-package-installed 'magit 'yasnippet 'projectile 'helm 'helm-projectile 'multiple-cursors 'dracula-theme 'monokai-theme 'groovy-mode 'meghanada 'dumb-jump 'go-mode 'protobuf-mode 'scala-mode 'sbt-mode 'ensime)
 
 (provide 'packages)
